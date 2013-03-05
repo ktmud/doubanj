bd26cbd87b7d96c09e347fc607858e41cbfe9b37
ktmud <jyyjcc@gmail.com>
2013-03-05 09:05:28 +0800

    ignore hash.json, change npm start

--- conf/development.conf.js
+++ conf/development.conf.js
@@ -4,11 +4,6 @@ var util = require('util');
  * DEVELOPMENT Environment settings
  */
 module.exports = {
-  debug: true,
+  debug: false,
   assets_root: 'http://127.0.0.1:3000/',
-  sessionStore: {
-    memcached: true,
-    dbname: 'sessions',
-    sync_time: 1 // minutes
-  }
 };
