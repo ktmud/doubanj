fdbee05d75073d67c10255940391e0518cb879ae
ktmud <jyyjcc@gmail.com>
2013-03-06 17:24:44 +0800

    update about contact link
    stop tracking production.conf

--- conf/production.conf.js
+++
@@ -1,7 +0,0 @@
-/**
-* when:
-*   $ export NODE_ENV=PRODUCTION
- */
-module.exports = {
-  raven: 'https://8d96b808c9be4afa80281b02b51c76bd:459bf76b7b8f42a38333ac661de3aa49@sentry.doubanj.com/3',
-};
