9e6c452d9a09e0b98ec4490c99a0632ceb14a72d
Jesse Yang <jyyjcc@gmail.com>
2013-03-08 14:03:52 +0800

    add a donate page

--- serve/misc.js
+++ serve/misc.js
@@ -3,4 +3,7 @@ module.exports = function(app, central) {
   app.get('/about', function(req, res, next) {
     res.render('misc/about');
   });
+  app.get('/donate', function(req, res, next) {
+    res.render('misc/donate');
+  });
 };
