8cb4388238d102a62cf2550b7db3186c901cece7
Jesse Yang <jyyjcc@gmail.com>
2013-03-13 10:57:03 +0800

    fix Model.ensured

--- conf/development.conf.tmpl.js
+++ conf/development.conf.tmpl.js
@@ -2,6 +2,11 @@
  * DEVELOPMENT Environment settings
  */
 module.exports = {
-  site_name: '豆瓣酱 - 开发模式'
-  //debug: false
+  site_name: '豆瓣酱 - 开发模式',
+  douban: {
+    limit: 30, // request limit per minute
+    key: '', // your douban api key (client_id)
+    secret: '' // your douban api secret
+  },
+  //debug: false,
 };
