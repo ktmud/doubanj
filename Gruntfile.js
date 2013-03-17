867f0ff7e589708a2be122b1b06062c689c2022f
Jesse Yang <jyyjcc@gmail.com>
2013-03-17 14:37:13 +0800

    update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  //grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.loadNpmTasks('grunt-hashmap');
