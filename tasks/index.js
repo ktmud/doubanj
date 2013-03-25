cd16ac51aa31c4bb3d9a8ef51f796209cde96345
Jesse Yang <jyyjcc@gmail.com>
2013-03-25 12:04:59 +0800

    make task queue as a standalone module: resumable

--- tasks/index.js
+++ tasks/index.js
@@ -7,12 +7,37 @@ var debug = require('debug');
 var verbose = debug('dbj:tasks:verbose');
 var log = debug('dbj:tasks:log');
 
-var TaskQueue = require('./queue');
+var Resumable = require('resumable');
 
 ['interest', 'compute'].forEach(function(item) {
   var mod = module.exports[item] = require('./' + item);
 
-  var queue = mod.queue = central['_' + item + '_queue'] = new TaskQueue('doubanj-queue-' + item, mod, redis.client);
+  var User = require('../models/user');
+
+  var queue = new Resumable({
+    key: 'doubanj-queue-' + item,
+    mod: mod,
+    storage: redis.client,
+    autoLoad: true,
+    ensure: function(list) {
+      var seen = {};
+      var ret = list.filter(function(arg) {
+        if (!arg[0] || !arg[1]) return false;
+        if (arg[1].user in seen) return false;
+        seen[arg[1].user] = 1;
+        return true;
+      });
+      return ret;
+    },
+    stringify: function(k, v) {
+      if (v instanceof User) {
+        return v.uid || v._id;
+      }
+      return v;
+    }
+  });
+
+  mod.queue = central['_' + item + '_queue'] = queue;
 
   // let the queue resume undone works.
   queue.on('ready', function(q) {
