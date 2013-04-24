479bc2c2ecb57f27a9c3c064bcc92e2e62c4bf5d
Jesse Yang <jyyjcc@gmail.com>
2013-04-24 10:37:55 +0800

    update tools update

--- tools/update.js
+++ tools/update.js
@@ -13,28 +13,41 @@ var oneday = 60 * 60 * 24 * 1000;
 function updateAll() {
   var now = new Date();
 
+  if (tasks.interest.queue.queue.length) {
+    log('There are unfinished task. Exit.');
+    return;
+  }
+
   User.stream(null, { limit: null }, function(stream) {
     stream.on('data', function(doc) {
       var u = new User(doc);
 
       // 一周之内更新过的用户就不再更新
-      //if (u.last_synced_status === 'ing' || now - u.last_synced < oneday * 7) {
-          //log('Skipping %s...', u.name);
-          //return;
-      //}
+      if (u.last_synced_status === 'ing' || now - u.last_synced < oneday * 7) {
+          log('Skipping %s...', u.name);
+          return;
+      }
 
       stream.pause();
 
-      tasks.interest.collect_book({
-        user: u, 
-        force: true,
-        fresh: true,
-        success: function() {
-          log('Callback sussess for user %s [%s].', u.uid, u.name);
-          if (stream.paused) {
-            stream.resume();
+      u.pull(function() {
+        tasks.interest.collect_book({
+          user: u, 
+          force: true,
+          fresh: false,
+          success: function() {
+            log('Callback sussess for user %s [%s].', u.uid, u.name);
+            if (stream.paused) {
+              stream.resume();
+            }
+          },
+          error: function() {
+            log('Callback error for user %s [%s].', u.uid, u.name);
+            if (stream.paused) {
+              stream.resume();
+            }
           }
-        },
+        });
       });
       log('Queue user %s [%s]', u.uid, u.name);
     });
@@ -45,4 +58,4 @@ function updateAll() {
   });
 }
 
-updateAll();
+setTimeout(updateAll, 2000);
