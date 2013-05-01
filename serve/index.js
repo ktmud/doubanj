08940cf55389e291e993e3ccd546ff75e894e445
Jesse Yang <jyyjcc@gmail.com>
2013-05-01 20:06:19 +0800

    登录用户直接输入id时的跳转; 移动网页优化

--- serve/index.js
+++ serve/index.js
@@ -1,20 +1,36 @@
 var utils = require('./utils');
 
 module.exports = function(app, central) {
-  app.all('/*', utils.navbar);
+
+  var User = require(central.cwd + '/models/user');
+
+  app.use(utils.navbar);
 
   app.get('/', function(req, res, next) {
     var uid = req.query.q;
     if (uid) {
       uid = utils.url2uid(uid);
-      if (uid) {
-        return res.redirect('/people/' + uid + '/');
-      } else {
-        return res.redirect('/');
+      if (!uid) return res.redirect('/');
+
+      req.uid = uid;
+
+      if (req.user && req.user.uid != uid) {
+        User.getFromMongo(uid, function(err, user) {
+          // 已经有统计结果，直接到契合指数页面
+          if (user && user.book_stats) {
+            return res.redirect(req.user.url() + 'click/' + user.uid);
+          }
+          next();
+        });
+        return;
       }
     }
+    next();
+  }, function(req, res, next) {
+    if (req.uid) return res.redirect('/people/' + req.uid + '/');
     res.render('index');
   });
+
   app.post('/', function(req, res, next) {
     var uid = utils.url2uid(req.body.uid);
     if (!uid) return res.redirect('/');
