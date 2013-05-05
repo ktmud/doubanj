a27edf9c532ada4468f7fbd1364258fbf92ff22f
Jesse Yang <jyyjcc@gmail.com>
2013-05-05 19:25:21 +0800

    增加一个删除 redis book_stats 的脚本

---
+++ tools/clean.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/**
+ * clean all redis stats cache for users
+ */
+var log = require('debug')('dbj:tool:update');
+
+var User = require('../models/user');
+
+var oneday = 60 * 60 * 24 * 1000;
+var oneweek = oneday * 7;
+
+function updateAll() {
+  var now = new Date();
+
+  User.stream({
+    stats_p: 100
+  }, { limit: null }, function(stream) {
+    stream.on('data', function(doc) {
+      var u = new User(doc);
+      log('Deleting stats cache for [%s]', u.name);
+      u._del_data('book_stats');
+    });
+    stream.on('close', function() {
+      log('=== Stream closed. ===');
+      setTimeout(process.exit, 200);
+    });
+  });
+}
+
+setTimeout(updateAll, 2000);
