3293f7c858f7cc24df2b6733d0227fc8d57fa5fd
Jesse Yang <jyyjcc@gmail.com>
2013-09-23 17:01:36 +0800

    really update bootstrap

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ require.register("main", function(exports, require, module) {
   // expose to window
   window.jQuery = window.$ = require('/jquery');
   window.Sizzle = $.find;
-  // @import ../components/ktmud-bootstrap/docs/assets/js/bootstrap.js
+  // @import ../components/ktmud-bootstrap/js/bootstrap.js
 });
 require.register("utils/datetime", function(exports, require, module) {
   // @import ./utils/datetime.js
