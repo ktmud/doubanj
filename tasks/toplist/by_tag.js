15f9839c25440a86f6d0069aa82e375822a1f34a
Jesse Yang <jyyjcc@gmail.com>
2014-02-27 10:54:00 +0800

    update fields

--- tasks/toplist/by_tag.js
+++ tasks/toplist/by_tag.js
@@ -1,100 +1,102 @@
-var central = require('../../lib/central');
-var mongo = central.mongo;
+var central = require('../../lib/central')
+var mongo = central.mongo
 
-var debug = require('debug');
-var log = debug('dbj:toplist:by_tag:log');
-var verbose = debug('dbj:toplist:by_tag:verbose');
-var error = debug('dbj:toplist:by_tag:error');
+var debug = require('debug')
+var log = debug('dbj:toplist:by_tag:log')
+var verbose = debug('dbj:toplist:by_tag:verbose')
+var error = debug('dbj:toplist:by_tag:error')
 
 // reduce to top 100 subjects
 //var reduce = function(tag, vals) {
-  //vals = Array.prototype.concat.apply([], vals);
+  //vals = Array.prototype.concat.apply([], vals)
   //vals = vals.sort(function(a, b) {
-    //return b.count - a.count;
-  //}).slice(0, 100);
+    //return b.count - a.count
+  //}).slice(0, 100)
 
-  //return vals;
-//};
+  //return vals
+//}
 
-var noop = function(){};
+var noop = function(){}
 
 function users_by_tag(ns, status, done) {
-  done = done || function(){};
+  done = done || function(){}
 
-  if (!ns || !status) return done(new Error('Namespace and status needed'));
+  if (!ns || !status) return done(new Error('Namespace and status needed'))
 
-  var stats_status = status || 'all';
-
-  var out_coll = ['top', ns, status, 'user_by_tag'].join('_');
+  var stats_status = status || 'all'
+  var out_coll = ['top', ns, status, 'user_by_tag'].join('_')
+  var ns_stats = ns + '_stats'
+  var fields = {}
+  var field_name = [ns_stats, stats_status, 'top_tags'].join('.')
+  fields[field_name] = 1
 
   mongo.queue(function(db, next) {
-    log('[%s] started...', out_coll);
+    log('[%s] started...', out_coll)
 
-    var query = { last_synced_status: 'succeed' };
+    var query = { last_synced_status: 'succeed' }
 
     // 至少总共读过50本书才可能进榜吧
     // { book_stats.done.total: { $gt: 50 } }
-    query[ns + '_stats.n_done'] = { $gt: 50 };
+    query[ns + '_stats.n_done'] = { $gt: 50 }
 
-    var stream = db.collection('user').find(query).stream();
+    var stream = db.collection('user').find(query, { fields: fields }).stream()
 
-    var out_collection = db.collection(out_coll);
+    var out_collection = db.collection(out_coll)
 
 
-    out_collection.ensureIndex({ tagname: 1, count: 1 }, { background: true }, noop);
+    out_collection.ensureIndex({ tagname: 1, count: 1 }, { background: true }, noop)
 
     function update_tags_coll(user_id, top_tags){
       top_tags.forEach(function(item, i) {
         out_collection.update({ _id: user_id + '::' + i }, {
           $set: { tagname: item._id, count: item.count }
-        }, { upsert: true, w: -1 });
-      });
+        }, { upsert: true, w: -1 })
+      })
     }
 
     stream.on('data', function(doc) {
-      if (!doc[ns + '_stats']) return;
-      var result = doc[ns + '_stats'][stats_status];
-      var top_tags = result && result.top_tags;
-
+      if (!doc[ns_stats]) return
+      var result = doc[ns_stats][stats_status]
+      var top_tags = result && result.top_tags
       if (top_tags) {
-        update_tags_coll(doc._id, top_tags);
+        update_tags_coll(doc._id, top_tags)
       }
-    });
+    })
 
     stream.once('error', function(err) {
-      next();
+      next()
       error('[%s] failed: ', out_coll, err)
-      done(err);
-    });
+      done(err)
+    })
 
     stream.once('close', function(err) {
-      next();
-      log('[%s] generated.', out_coll);
-      done();
-    });
-  }, 4);
+      next()
+      log('[%s] generated.', out_coll)
+      done()
+    })
+  }, 4)
 }
 
 function subjects_by_tag(ns, done) {
-  done = done || function(){};
+  done = done || function(){}
 
-  if (!ns) return done(new Error('Namespace needed'));
+  if (!ns) return done(new Error('Namespace needed'))
 
-  var out_coll = ['top', ns, 'by_tag'].join('_');
+  var out_coll = ['top', ns, 'by_tag'].join('_')
 
   mongo.queue(function(db, next) {
-    log('[%s] started...', out_coll);
+    log('[%s] started...', out_coll)
 
     // 至少有十个人打分的图书才有可能进入热门榜吧
     var query = {
       'raters': { $gt: 10 }
-    };
+    }
 
-    var stream = db.collection(ns).find(query).stream();
+    var stream = db.collection(ns).find(query, { fields: {tags: 1} }).stream()
 
-    var out_collection = db.collection(out_coll);
+    var out_collection = db.collection(out_coll)
 
-    out_collection.ensureIndex({ tagname: 1, count: 1 }, { background: true }, noop);
+    out_collection.ensureIndex({ tagname: 1, count: 1 }, { background: true }, noop)
 
     function update_tags_coll(subject_id, top_tags){
       top_tags.forEach(function(item, i) {
@@ -102,30 +104,30 @@ function subjects_by_tag(ns, done) {
           // 豆瓣API返回的 tag 格式为
           // { name: 'xxx', title: 'xxx', count: 10 }
           $set: { tagname: item.name, count: item.count }
-        }, { upsert: true, w: -1 });
-      });
+        }, { upsert: true, w: -1 })
+      })
     }
 
     stream.on('data', function(doc) {
-      if (!doc.tags) return;
-      update_tags_coll(doc._id, doc.tags);
-    });
+      if (!doc.tags) return
+      update_tags_coll(doc._id, doc.tags)
+    })
 
     stream.on('error', function(err) {
-      next();
+      next()
       error('[%s] failed: ', out_coll)
-      done(err);
-    });
+      done(err)
+    })
 
     stream.on('close', function(err) {
-      next();
-      log('[%s] generated.', out_coll);
-      done();
-    });
-  }, 4);
+      next()
+      log('[%s] generated.', out_coll)
+      done()
+    })
+  }, 4)
 }
 
 module.exports = {
   users: users_by_tag,
   subjects: subjects_by_tag,
-};
+}
