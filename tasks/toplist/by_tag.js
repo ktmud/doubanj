b459dbfe843a96a73d70e95a7391036b21fa4820
Jesse Yang <jyyjcc@gmail.com>
2014-03-04 17:02:58 +0800

    use batch stream for balk insert

--- tasks/toplist/by_tag.js
+++ tasks/toplist/by_tag.js
@@ -1,4 +1,6 @@
 var central = require('../../lib/central')
+var _ = require('../../lib/utils').lodash
+var Batcher = require('batch-stream2')
 var mongo = central.mongo
 
 var debug = require('debug')
@@ -15,42 +17,80 @@ var error = debug('dbj:toplist:by_tag:error')
 
   //return vals
 //}
-
 var noop = function(){}
 
+
+function createBatcher(out_collection) {
+  var buffer = new Batcher({
+    size: 5000,
+    timeout: 5000,
+    transform: function(items, callback) {
+      var ids = _.pluck(items, '_id')
+      // bulk remove, then bulk insert
+      out_collection.remove({_id: {$in: ids }}, function(err) {
+        if (err) {
+          console.error(err)
+        }
+        out_collection.insert(items, { continueOnError: true }, function(err) {
+          if (err) {
+            console.error(err)
+          } else {
+            log('[%s] %s items saved.', out_collection.collectionName, items.length)
+          }
+          callback()
+        })
+      })
+    }
+  })
+  // manual resume
+  buffer.resume()
+  return buffer
+}
+
 function users_by_tag(ns, status, done) {
-  done = done || function(){}
+  done = done || noop
 
   if (!ns || !status) return done(new Error('Namespace and status needed'))
 
   var stats_status = status || 'all'
   var out_coll = ['top', ns, status, 'user_by_tag'].join('_')
   var ns_stats = ns + '_stats'
-  var fields = {}
   var field_name = [ns_stats, stats_status, 'top_tags'].join('.')
+
+  var query = { last_synced_status: 'succeed' }
+  // 至少总共读过50本书才可能进榜吧
+  // { book_stats.done.total: { $gt: 50 } }
+  query[ns + '_stats.n_done'] = { $gt: 50 }
+
+  var fields = {}
   fields[field_name] = 1
 
   mongo.queue(function(db, next) {
     log('[%s] started...', out_coll)
 
-    var query = { last_synced_status: 'succeed' }
-
-    // 至少总共读过50本书才可能进榜吧
-    // { book_stats.done.total: { $gt: 50 } }
-    query[ns + '_stats.n_done'] = { $gt: 50 }
-
-    var stream = db.collection('user').find(query, { fields: fields }).stream()
-
     var out_collection = db.collection(out_coll)
+    var stream = db.collection('user').find(query, { fields: fields }).stream()
+    var buffer = createBatcher(out_collection)
 
+    function callback(err) {
+      if (err) {
+        error('[%s] failed: ', out_coll, err)
+      } else {
+        log('[%s] generated.', out_coll)
+      }
+      next()
+      done(err)
+    }
 
     out_collection.ensureIndex({ tagname: 1, count: 1 }, { background: true }, noop)
 
     function update_tags_coll(user_id, top_tags){
       top_tags.forEach(function(item, i) {
-        out_collection.update({ _id: user_id + '::' + i }, {
-          $set: { tagname: item._id, count: item.count }
-        }, { upsert: true, w: -1 })
+        buffer.write({
+          _id: user_id + '::' + i,
+          tagname: item._id,
+          count: item.count
+        })
       })
     }
 
@@ -62,18 +102,11 @@ function users_by_tag(ns, status, done) {
         update_tags_coll(doc._id, top_tags)
       }
     })
-
-    stream.once('error', function(err) {
-      next()
-      error('[%s] failed: ', out_coll, err)
-      done(err)
+    stream.once('close', function() {
+      buffer.end()
     })
-
-    stream.once('close', function(err) {
-      next()
-      log('[%s] generated.', out_coll)
-      done()
-    })
+    stream.once('error', callback)
+    buffer.once('end', callback)
   }, 4)
 }
 
@@ -83,28 +116,39 @@ function subjects_by_tag(ns, done) {
   if (!ns) return done(new Error('Namespace needed'))
 
   var out_coll = ['top', ns, 'by_tag'].join('_')
+  // 至少有十个人打分的图书才有可能进入热门榜吧
+  var query = { 'raters': { $gt: 10 } }
 
   mongo.queue(function(db, next) {
     log('[%s] started...', out_coll)
 
-    // 至少有十个人打分的图书才有可能进入热门榜吧
-    var query = {
-      'raters': { $gt: 10 }
-    }
-
-    var stream = db.collection(ns).find(query, { fields: {tags: 1} }).stream()
-
     var out_collection = db.collection(out_coll)
+    var stream = db.collection(ns).find(query, { fields: {tags: 1} }).stream()
+    var buffer = createBatcher(out_collection)
+    var total = 0
 
     out_collection.ensureIndex({ tagname: 1, count: 1 }, { background: true }, noop)
 
+    function callback(err) {
+      if (err) {
+        error('[%s] failed: ', out_coll, err)
+      } else {
+        log('[%s] generated.', out_coll)
+      }
+      next()
+      done(err)
+    }
+
     function update_tags_coll(subject_id, top_tags){
       top_tags.forEach(function(item, i) {
-        out_collection.update({ _id: subject_id + '::' + i }, {
-          // 豆瓣API返回的 tag 格式为
-          // { name: 'xxx', title: 'xxx', count: 10 }
-          $set: { tagname: item.name, count: item.count }
-        }, { upsert: true, w: -1 })
+        total++;
+        // 豆瓣API返回的 tag 格式为
+        // item = { name: 'xxx', title: 'xxx', count: 10 }
+        buffer.write({
+          _id: subject_id + '::' + i,
+          tagname: item.name,
+          count: item.count
+        })
       })
     }
 
@@ -112,17 +156,13 @@ function subjects_by_tag(ns, done) {
       if (!doc.tags) return
       update_tags_coll(doc._id, doc.tags)
     })
-
-    stream.on('error', function(err) {
-      next()
-      error('[%s] failed: ', out_coll)
-      done(err)
+    stream.once('close', function() {
+      buffer.end()
     })
-
-    stream.on('close', function(err) {
-      next()
-      log('[%s] generated.', out_coll)
-      done()
+    stream.once('error', callback)
+    buffer.once('end', function() {
+      log('[%s] total: %s', out_coll, total)
+      callback()
     })
   }, 4)
 }
